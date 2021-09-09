83e6d9b0165e3a61f1b6271c3cf3306dacf3ff36
Ediri99 <bhanukaediri99@gmail.com>
2021-09-09 19:09:26 +0530

    employee table

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import AdminNavbar from './components/AdminNavbar';
-import { BrowserRouter  as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './App.css';
+//import {BrowserRouter as Router, Route, Switch}from 'react-router-dom';
 import AdminHome from './pages/AdminHome';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-    <Router>
-      <AdminNavbar/>
-      <Switch>
-        <Route path = '/' exact component = {AdminHome} />
-        <Route path = '/Employee'component = {ListEmployeeComponent} /> 
-      </Switch>
-      </Router>
-    </>
+
+    <div>
+      <>
+        <Router>
+          <AdminNavbar/>
+          <Switch>
+            <Route path = '/' exact component = {AdminHome} />
+            <Route path = '/Employee'component = {ListEmployeeComponent} /> 
+          </Switch>
+          <Footer/>
+        </Router>
+      </>
+    </div>
+    
   );
 }
 
--- Frontend/src/components/ListEmployeeComponent.jsx
+++ Frontend/src/components/ListEmployeeComponent.jsx
@@ -16,35 +16,37 @@ class ListEmployeeComponent extends Component {
             this.setState({employees:res.data});
         });
     }
-    
+
     render() {
         return (
             <div>
-                <h2 className="text-center">Employees List</h2>
+                <h2 className="text-center">Employees List</h2><br/><br/>
                 <div className = "row">
+                    <center>
                     <table className = "table table-striped table-bordered">
                         <thead>
                             <tr>
-                                <th>Employee First Name</th>
-                                <th>Employee Last Name</th>
-                                <th>Employee Email Address</th>
-                                <th>actions</th>
+                                <th><center>Employee First Name</center></th>
+                                <th><center>Employee Last Name</center></th>
+                                <th><center>Employee Email Address</center></th>
+                                <th><center>Actions</center></th>
                             </tr>
+                        </thead>
 
                             <tbody>
                                 {
                                     this.state.employees.map(
-                                        employee =>
-                                        <tr key = {employee.id}>
-                                            <td>{employee.firstName}</td>
-                                            <td>{employee.lastName}</td>
-                                            <td>{employee.emailId}</td>
+                                        employees =>
+                                        <tr key = {employees.id}>
+                                            <td>{employees.firstName}</td>
+                                            <td>{employees.lastName}</td>
+                                            <td>{employees.emailId}</td>
                                         </tr>
                                     )
                                 }
                             </tbody>
-                        </thead>
                     </table>
+                    </center>
 
                 </div>
             </div>
