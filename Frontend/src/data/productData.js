325fcce138cc9bd5a5889d9ec19515bf73fc88a5
Sumudu Himasha Ranaweera <it20251000@my.sliit.lk>
2021-10-01 23:41:18 +0530

    Products

--- Frontend/src/data/productData.js
+++ Frontend/src/data/productData.js
@@ -1,5 +1,57 @@
 const product_card = [
     {
-        id = 1
+        id = 1,
+        product_name: 'Canon EOS 5D Mark IV DSLR Camera',
+        description:'SKU #101852S1 MODEL #1483C002 UPC #013803281347',
+        price: 849500,
+        currency: "Rs",
+        thumb: "./images/ProductImages/Canon EOS 5D Mark IV DSLR Camera.png"
+    },
+
+    {
+        id = 2,
+        product_name: 'Canon EOS 80D DSLR Camera',
+        description:'SKU #101846S1 UPC #013803271829',
+        price: 214500,
+        currency: "Rs",
+        thumb: "./images/ProductImages/Canon EOS 80D DSLR Camera with 18-55mm Lens.png"
+    },
+    
+    {
+        id = 3,
+        product_name: 'Canon EOS 80D DSLR Camera',
+        description:'SKU #101846S1 UPC #013803271829',
+        price: 214500,
+        currency: "Rs",
+        thumb: "./images/ProductImages/Canon EOS 80D DSLR Camera with 18-135mm Lens.png"
+    },
+    
+    {
+        id = 4,
+        product_name: 'Canon EOS 90D DSLR Camera',
+        description:'SKU #101998S1 MODEL #3616C002 UPC #013803316186',
+        price: 315500,
+        currency: "Rs",
+        thumb: "./images/ProductImages/Canon EOS 90D DSLR Camera.png"
+    },
+
+    {
+        id = 5,
+        product_name: 'Canon EOS 2000D DSLR Camera',
+        description:'SKU #101997S1 UPC #013803302721',
+        price: 99500,
+        currency: "Rs",
+        thumb: "./images/ProductImages/Canon EOS 2000D DSLR Camera.png"
+    },
+
+    {
+        id = 6,
+        product_name: 'Canon EOS 4000D DSLR Camera',
+        description:'SKU #101862S1',
+        price: 98500,
+        currency: "Rs",
+        thumb: "./images/ProductImages/Canon EOS 4000D DSLR Camera.png"
     }
+    
+
 ]
