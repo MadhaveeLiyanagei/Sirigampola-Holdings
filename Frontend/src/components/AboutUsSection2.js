16f642b0dbad9a0c2c6013721b99891327a447c1
Ashan <ashanekanayaka099@gmail.com>
2021-10-04 17:37:56 +0530

    about us new images

--- Frontend/src/components/AboutUsSection2.js
+++ Frontend/src/components/AboutUsSection2.js
@@ -20,8 +20,8 @@ function AboutUsSection2() {
             <p>Sirigampola Holdings is a digital B2B marketplace aiming to be a comprehensive international trade hub for Small and Medium - sized Enterprises (SMEs) and their counterparts.
                 Empowered by our proprietary blockchain technology, Export Portal prioritizes security, transparency, cost-effectiveness, and ease-of-use. Thus, our partners can confidently trade, network, and communicate with other verified companies and experts from all over the world. With Export Portal, SMEs have an exponential growth opportunity – not just geographically, but within their verticals as well.</p>
             </div>
-            <div className="img">
-                  <img src="./images/about-us.jpg" alt="logo" />
+            <div>
+                  <img className="aboutusimgwhat" src="./images/about-us.jpg" alt="logo" />
                   </div>
            <div className= "about-us__heading">
            <div className="about-us__ttl ">
@@ -37,18 +37,74 @@ function AboutUsSection2() {
                </div> 
 
                <div>
-                   <table><tbody><td>
+                   <table>
+                       <tbody>
+                           <td>
                        <th>
+                       <img className="securityimg" src="./images/security.png" alt="logo" />
+                           <h2 className="bold_color">Total Security</h2>
+                    <p>We are hyper-focused on security. To maintain the utmost levels of safety, we start by verifying every user on our platform, ensuring their business is legitimate and up to date.
+                    Strong</p>
 
+        <br></br>
+<                       img className="securityimg" src="./images/protect.png" alt="logo" />
+                     <h2 className="bold_color">Strong Protection</h2>
+                    <p>Our blockchain technology is truly game-changing – never before could trade stakeholders communicate and share sensitive data so quickly and securely.</p>
+        
+
+        <br></br>
+<                       img className="securityimg" src="./images/box.png" alt="logo" />
+                     <h2 className="bold_color">Professional Expertise</h2>
+                    <p>EP+, or Export Portal’s Expert Panel, is a collaborative effort between industry experts and the Export Portal team to provide our users with the information they need.</p>
 
                            </th>
+                        
                            <th>
-                               
+                          
+                           <img className="aboutusimgsecurity"src="./images/about_us_inventory.jpg" alt="logo" />
+                            
                            </th>
                            
-                    </td></tbody></table>
+                          </td>
+                    </tbody>
+                </table>
+
+                    <br></br>
+                    <center>
+                    <h1>Trusted by over 10,000 Customers island-wide</h1>
+                    </center>
+                    <br></br>
                </div>
-  
+
+               <div>
+                   <table>
+                       <tbody>
+                           <tb>
+                                <th>            <br></br>
+                                     <img className="about_inventory"src="./images/about_inventory.jpg" alt="logo" />
+                                </th>
+
+                                <th>
+                                <br></br>
+                                <img className="securityimg" src="./images/security.png" alt="logo" />
+                           <h2><b className="bold_color" >1+</b> years</h2>
+                    <p>Responding to an industry need, Export Portal started working 6 years ago to provide SMEs with a new, expedited, and secure way to trade internationally.</p>
+
+        <br></br>
+        <img className="securityimg" src="./images/security.png" alt="logo" />
+                           <h2><b className="bold_color" >Island</b>-Wide</h2>
+                    <p>We have a truly global presence, with users in 54 countries and growing. With our international coverage, we can provide service to buyers, sellers, manufacturers, and freight forwarders all over.</p>
+
+        <br></br>
+
+
+
+                                </th>
+                            </tb>    
+                        </tbody>
+                    </table>
+               </div>
+               
   
           
       </div>
